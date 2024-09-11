7b3fabd1f2bbdd8882da5c9b69484d78226af53f
syedzohaibalikazmi <syedzohaibali9900@gmail.com>
2024-09-12 02:55:34 +0500

    Clothing-Store

--- E-commerce/src/components/Account.jsx
+++ E-commerce/src/components/Account.jsx
@@ -1,18 +1,34 @@
+import { useContext } from "react";
+import { UserContext } from "../context/UserContext";
 
+function Account() {
+  const { user } = useContext(UserContext);
 
-import { useContext } from "react"
-import { UserContext } from "../context/UserContext"
+  return (
+    <div
+      id="main"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignitems: "center",
+        width: "auto",
+        height: "20px",
+        backgroundColor: "black",
+        padding: "10px 0px",
+      }}
+    >
+      {user.isLogin ? (
+        <p style={{ color: "#969696" }}>
+          Welcome
+          <span style={{ color: "#e3e3e3", paddingLeft: "10px", fontSize: "18px" }}>
+            "{user.email}"
+          </span>
+        </p>
+      ) : (
+        <p style={{ color: "#CCCCCC" }}>Create your Account</p>
+      )}
+    </div>
+  );
+}
 
-function Account () {
-
-  const {user}=  useContext(UserContext)
-
-    return(
-        <div id="main" style={{ display: "flex", justifyContent: "center",alignitems: "center",width: "auto",height: "20px",backgroundColor: "black"}}>
-        {user.isLogin ? (<p style={{color:'#CCCCCC', marginBottom:"10px"}}>Welcome {user.email}</p>):null}    
-        </div>
-    )
-    
-} 
-
-export default Account
+export default Account;
